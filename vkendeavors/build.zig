ca0591f4511124ddbd751206286b58fbb08829ca
Justin Braben <jbraben@dmsiworks.com>
2024-01-17 15:24:34 -0700

    Initial set up for vulkan zig project

--- vkendeavors/build.zig
+++ vkendeavors/build.zig
@@ -5,15 +5,6 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "vkendeavors",
-        .root_source_file = .{ .path = "src/root.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(lib);
-
     const exe = b.addExecutable(.{
         .name = "vkendeavors",
         .root_source_file = .{ .path = "src/main.zig" },
@@ -21,7 +12,77 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const vk_lib_name = if (target.result.os.tag == .windows) "vulkan-1" else "vulkan";
+
+    exe.linkSystemLibrary("SDL3");
+    exe.linkSystemLibrary(vk_lib_name);
+    exe.addLibraryPath(.{ .cwd_relative = "thirdparty/SDL3/lib" });
+    exe.addIncludePath(.{ .cwd_relative = "thirdparty/sdl3/include" });
+
+    if (b.env_map.get("VK_SDK_PATH")) |path| {
+        // VK_SDK_PATH is set, you can use it
+        std.debug.print("VK_SDK_PATH is set: {s}\n", .{path});
+
+        exe.addLibraryPath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/Lib", .{path}) catch @panic("OOM") });
+        exe.addIncludePath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/Include", .{path}) catch @panic("OOM") });
+    } else {
+        // VK_SDK_PATH is not set
+        std.debug.print("VK_SDK_PATH environment variable is not set. Vulkan headers may not be found.\n", .{});
+        // You may want to handle this case or provide instructions to the user
+    }
+    exe.addCSourceFile(.{ .file = .{ .path = "src/vk_mem_alloc.cpp" }, .flags = &.{""} });
+    exe.addIncludePath(.{ .path = "thirdparty/vma/" });
+    exe.addCSourceFile(.{ .file = .{ .path = "src/stb_image.c" }, .flags = &.{""} });
+    exe.addIncludePath(.{ .path = "thirdparty/stb/" });
+    exe.addIncludePath(.{ .path = "thirdparty/imgui/" });
+
+    exe.linkLibCpp();
+
+    // TODO: compile shaders here
+
     b.installArtifact(exe);
+    switch (target.result.os.tag) {
+        .windows => {
+            b.installBinFile("thirdparty/sdl3/lib/SDL3.dll", "SDL3.dll");
+        },
+        else => {
+            b.installBinFile("thirdparty/sdl3/lib/libSDL3.so", "libSDL3.so.0");
+            exe.root_module.addRPathSpecial("$ORIGIN");
+        },
+    }
+
+    // Imgui (with cimgui and vulkan + sdl3 backends)
+    const imgui_lib = b.addStaticLibrary(.{
+        .name = "cimgui",
+        .target = target,
+        .optimize = optimize,
+    });
+    imgui_lib.addIncludePath(.{ .path = "thirdparty/imgui/" });
+    imgui_lib.addIncludePath(.{ .path = "thirdparty/sdl3/include/" });
+    if (b.env_map.get("VK_SDK_PATH")) |path| {
+        imgui_lib.addLibraryPath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/Lib", .{path}) catch @panic("OOM") });
+        imgui_lib.addIncludePath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/Include", .{path}) catch @panic("OOM") });
+    } else {
+        // VK_SDK_PATH is not set
+        std.debug.print("VK_SDK_PATH environment variable is not set. Vulkan headers may not be found.\n", .{});
+        // You may want to handle this case or provide instructions to the user
+    }
+    imgui_lib.linkLibCpp();
+    imgui_lib.addCSourceFiles(.{
+        .files = &.{
+            "thirdparty/imgui/imgui.cpp",
+            "thirdparty/imgui/imgui_demo.cpp",
+            "thirdparty/imgui/imgui_draw.cpp",
+            "thirdparty/imgui/imgui_tables.cpp",
+            "thirdparty/imgui/imgui_widgets.cpp",
+            "thirdparty/imgui/imgui_impl_sdl3.cpp",
+            "thirdparty/imgui/imgui_impl_vulkan.cpp",
+            "thirdparty/imgui/cimgui.cpp",
+            "thirdparty/imgui/cimgui_impl_sdl3.cpp",
+            "thirdparty/imgui/cimgui_impl_vulkan.cpp",
+        },
+    });
+    exe.linkLibrary(imgui_lib);
 
     const run_cmd = b.addRunArtifact(exe);
 
